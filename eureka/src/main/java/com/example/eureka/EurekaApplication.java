f25ca0e1369414c28677ff9d066adf589e04dbce
jinguibo <jinguibo@postop.cn>
2020-09-11 17:46:34 +0800

    [change]上传代码
    Signed-off-by: jinguibo <jinguibo@postop.cn>

--- eureka/src/main/java/com/example/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/example/eureka/EurekaApplication.java
@@ -1,18 +1,23 @@
 package com.example.eureka;
 
 import com.example.eureka.extend.MySpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
 
-	public static void main(String[] args) {
+	@Autowired
+	Environment environment;
 
+	public static void main(String[] args) {
 		SpringApplication application = new MySpringApplication(EurekaApplication.class);
 		application.run(args);
+
 	}
 
 }
