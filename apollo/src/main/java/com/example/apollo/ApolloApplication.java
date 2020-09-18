a184beeee4c456fb9ce3c0de77bd7837663ceef7
jinguibo <jinguibo@postop.cn>
2020-09-18 17:56:05 +0800

    [change]上传代码
    Signed-off-by: jinguibo <jinguibo@postop.cn>

--- apollo/src/main/java/com/example/apollo/ApolloApplication.java
+++ apollo/src/main/java/com/example/apollo/ApolloApplication.java
@@ -1,13 +1,31 @@
 package com.example.apollo;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableApolloConfig
+@RestController
 public class ApolloApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApolloApplication.class, args);
 	}
 
+	@Value("${jgbTest:JJJ}")
+	String test;
+	@Value("${jgb.apollo.test.name:null}")
+	String testName;
+	@Value("${jgb.apollo.test.address:null}")
+	String address;
+
+	@GetMapping("/test/apollo")
+	public String getTestApollo() {
+
+		return address;
+	}
 }
