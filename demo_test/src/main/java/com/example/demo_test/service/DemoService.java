e84a78173c7c9a47491484072c3a7a468b25a4ff
“金桂波” <jingb@newhope.cn>
2020-12-08 14:09:21 +0800

    [change]提交代码

--- demo_test/src/main/java/com/example/demo_test/service/DemoService.java
+++ demo_test/src/main/java/com/example/demo_test/service/DemoService.java
@@ -12,30 +12,4 @@ public class DemoService {
 
 		return "service";
 	}
-
-	public static void main(String[] args) {
-		String input = "[I love byte bytedance] Ilovebytedance";
-		String[] split = input.split("]");
-		// 获取所有单词字典
-		String strs = split[0].substring(1,split[0].length()-1);
-		List<String> allDict = Arrays.asList(strs.split(" "));
-
-		// 获取匹配字符串
-		String matchStr = split[1].trim();
-
-		System.out.println(allDict);
-		System.out.println(matchStr);
-
-		// 匹配
-
-	}
-
-	public boolean match(String matchStr,List<String> dict) {
-		// 校验
-
-		// 匹配算法
-
-
-		return false;
-	}
 }
